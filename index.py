1a4cd1f1df2e216e30e2e7b64cc7842b94d3cb3e
MG Hirsch <mghirsch@umd.edu>
2024-08-23 19:33:22 -0400

    remove scipy

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 import warnings
-from scipy.integrate import odeint
 import sys
 from dash import Dash, dcc, callback, Input, Output, html
 from plotly import express as px
@@ -18,9 +17,13 @@ class MainApplication:
 
     def set_layout(self):
         # Initial figure
-        sol_c1, sol_c11 = self.run(0.135, 0.11, 0.15, -0.2, -0.5, -0.1, 10, 30, 0.1, 0.9)
-        t = list(range(0, 30, 1))
-        fig = px.line(x=t, y=[sol_c1, sol_c11])  
+        sol_t, sol_c1, sol_c11 = self.run(0.135, 0.11, 0.15, -0.2, -0.5, -0.1, 10, 30, 0.1, 0.9)
+        # print(sol_t)
+        # print(sol_c1)
+        # print(sol_c11)
+        fig = px.line(x=sol_t, y=[sol_c1, sol_c11])  
+        fig.data[0].name = "C1"
+        fig.data[1].name = "C11"
 
         self.app.layout = [
             dcc.Graph(figure=fig, id="plot"),
@@ -59,43 +62,64 @@ class MainApplication:
         Input(component_id="c11_slider", component_property="value")
         )
         def update_graph(g1, g11, nude_k, nude_m, b6_k, b6_m, switch_time, end_time, c1, c11):
-            sol_c1, sol_c11 = self.run(g1, g11, nude_k, nude_m, b6_k, b6_m, switch_time, end_time, c1, c11)
-            t = list(range(0, end_time, 1))
-            fig = px.line(x=t, y=[sol_c1, sol_c11])
+            sol_t, sol_c1, sol_c11 = self.run(g1, g11, nude_k, nude_m, b6_k, b6_m, switch_time, end_time, c1, c11)
+            fig = px.line(x=sol_t, y=[sol_c1, sol_c11])#, labels={"wide_variable_0": "C1", "wide_variable_1": "C11"})
+            fig.data[0].name = "C1"
+            fig.data[0].legendgroup = "C1"
+            fig.data[0].hovertemplate = fig.data[0].hovertemplate.replace("wide_variable_0", "C1")
+            fig.data[1].name = "C11"
+            fig.data[1].legendgroup = "C11"
+            fig.data[1].hovertemplate = fig.data[0].hovertemplate.replace("wide_variable_1", "C11")
+            fig.update_layout(legend_title = "Subline")
             return fig
 
-
     # Game defined as change in each population at each time step
-    def game(self, x, t, g1, g11, k, m):
-        c1 = x[0]
-        c11 = x[1]
-        dxdt = [(g1 + k*c11) * c1,
-                (g11 + m*c1) * c11]
-        return dxdt
+    # (Yes, some parameters are worthless, leaving for now for consistency)
+    def game_c1(self, t, c1, c11, g1, g11, k, m):
+        return (g1 + k*c11) * c1
+    def game_c11(self, t, c1, c11, g1, g11, k, m):
+        return (g11 + m*c1) * c11
+    
+    def RungeKutta(self, x, y, z, dx, dydx, dzdx, g1, g11, k, m):
+        # From https://primer-computational-mathematics.github.io/book/c_mathematics/numerical_methods/5_Runge_Kutta_method.html
+        # x = time, y = c1, z = c11
+        k1 = dx*dydx(x, y, z, g1, g11, k, m)
+        h1 = dx*dzdx(x, y, z, g1, g11, k, m)
+        k2 = dx*dydx(x+dx/2., y+k1/2., z+h1/2., g1, g11, k, m)
+        h2 = dx*dzdx(x+dx/2., y+k1/2., z+h1/2., g1, g11, k, m)
+        k3 = dx*dydx(x+dx/2., y+k2/2., z+h2/2., g1, g11, k, m)
+        h3 = dx*dzdx(x+dx/2., y+k2/2., z+h2/2., g1, g11, k, m)
+        k4 = dx*dydx(x+dx, y+k3, z+h3, g1, g11, k, m)
+        h4 = dx*dzdx(x+dx, y+k3, z+h3, g1, g11, k, m)
+
+        y = y + 1./6.*(k1+2*k2+2*k3+k4)
+        z = z + 1./6.*(h1+2*h2+2*h3+h4)
+        x = x + dx
+        
+        return x, y, z
 
     # Method to estimate the game over time. Error catching for when a solution can't be reached.
     # If a solution can't be reached, reduce the time scale and try again.
-    def est_ode(self, game, init, max_time, g1, g11, k, m):
-        t = list(range(0, max_time, 1))
-        success = False
-        while(success==False):
-            with warnings.catch_warnings(record=True):
-                sol = odeint(game, init, t, args=(g1, g11, k, m), full_output=True)
-                if sol[1]["message"] == "Integration successful.": 
-                    success = True
-                    sol = sol[0]
-                    sol[sol < 0] = 0
-                    return sol
-                if len(t) > 10:
-                    t = t[:-2]
-                else: return -sys.maxsize
+    def est_ode(self, c1, c11, max_time, g1, g11, k, m):
+        # From https://primer-computational-mathematics.github.io/book/c_mathematics/numerical_methods/5_Runge_Kutta_method.html
+        t = 0
+        t_list = [t]
+        c1_list = [c1]
+        c11_list = [c11]
+        while t < max_time:
+            t, c1, c11 = self.RungeKutta(t, c1, c11, .1, self.game_c1, self.game_c11, g1, g11, k, m)
+            t_list.append(t)
+            c1_list.append(c1)
+            c11_list.append(c11)
+        return t_list, c1_list, c11_list
 
     def run(self, g1, g11, nude_k, nude_m, b6_k, b6_m, switch_time, end_time, c1_init, c11_init):
         # Run the game with nude parameters up to time to switch
-        sol1 = self.est_ode(self.game, [c1_init, c11_init], switch_time+1, g1, g11, nude_k, nude_m)
+        sol1_t, sol1_c1, sol1_c11 = self.est_ode(c1_init, c11_init, switch_time, g1, g11, nude_k, nude_m)
         # Change k and m and continue the game
-        sol2 = self.est_ode(self.game, [sol1[-1,0], sol1[-1,1]], end_time-switch_time, g1, g11, b6_k, b6_m)
-        return sol1[:,0].tolist() + sol2[1:,0].tolist(), sol1[:,1].tolist() + sol2[1:,1].tolist()
+        sol2_t, sol2_c1, sol2_c11 = self.est_ode(sol1_c1[-1], sol1_c11[-1], end_time-switch_time, g1, g11, b6_k, b6_m)
+        sol2_t = [sol1_t[-1] + i for i in sol2_t]
+        return sol1_t + sol2_t, sol1_c1 + sol2_c1, sol1_c11 + sol2_c11
 
 Application = MainApplication()
 app = Application.app
